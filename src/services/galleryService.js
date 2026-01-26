d07ce50dd9875ac972b7a9f11255d14da0f8623c
saqeeb66 <166957026+saqeeb66@users.noreply.github.com>
2026-01-26 14:39:51 +0530

    Update galleryService.js

--- src/services/galleryService.js
+++ src/services/galleryService.js
@@ -1,20 +1,24 @@
-import API from "./api";
+import api from "./api";
 
-// Public: get gallery images
-export const getGalleryImages = () => {
-  return API.get("/gallery");
+// Public – fetch gallery (images + videos)
+export const getGallery = async () => {
+  const res = await api.get("/gallery");
+  return res.data;
 };
 
-// Admin: upload image (Cloudinary)
-export const uploadGalleryImage = (formData) => {
-  return API.post("/gallery", formData, {
+// Admin – upload image or video
+export const uploadGalleryFile = async (formData) => {
+  const res = await api.post("/gallery", formData, {
     headers: {
       "Content-Type": "multipart/form-data",
     },
   });
+  return res.data;
 };
 
-// Admin: delete image
-export const deleteGalleryImage = (id) => {
-  return API.delete(`/gallery/${id}`);
+// Admin – delete image or video
+export const deleteGalleryFile = async (id) => {
+  const res = await api.delete(`/gallery/${id}`);
+  return res.data;
 };
+
